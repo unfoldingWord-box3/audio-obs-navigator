373f696cd6fc788fce2e7e4a699cb79243d9b4b6
kintsoogi <kintsoogii@proton.me>
2024-03-07 12:00:48 -0500

    Command retrieval from chroma db based on user input working

---
+++ python_llm/main.py
@@ -0,0 +1,35 @@
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain_community.document_loaders import JSONLoader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", default="Open OBS story 9 frame 4")
+args = parser.parse_args()
+
+embeddings = OpenAIEmbeddings()
+
+loader = JSONLoader(
+    file_path='commands.json',
+    jq_schema='.[].variants[]',
+    text_content=False)
+
+command_docs = loader.load()
+
+db = Chroma.from_documents(
+  command_docs,
+  embedding=embeddings,
+  persist_directory="emb"
+)
+
+# print(db)
+
+results = db.similarity_search(
+  "Go to story 7 frame 2"
+)
+
+for result in results:
+  print("\n")
+  print(result.page_content)
